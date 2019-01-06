47f51afc3d4891eb72ff0c470adb118546cb7256
tschneider <tschneider@live.com>
2019-01-05 21:55:59 -0500

    #574 - Initial checkin of the ForemanApiWrapper library. I have created unittests to show that environments can be created.

---
+++ ForemanApiWrapper.py
@@ -0,0 +1,42 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+class ForemanApiWrapper():
+
+    ErrorMessage_ApiCall = "An error occurred while making an API call."
+
+    def __init__(self, username, password, url, verifySsl):
+        self.Username = username
+        self.Password = password
+        self.Auth = HTTPBasicAuth(username , password)
+        self.Url = url
+        self.VerifySsl = verifySsl
+
+    def MakeApiCall(self, endpoint, method, arguments=None):
+
+        try:
+            # Make the api call
+            m = str.lower(method)
+            func = getattr(requests, m)
+            url = self.Url + endpoint
+
+            results = None
+            if arguments:
+                results = func(url, auth=self.Auth, verify=self.VerifySsl, json=arguments)
+            else:
+                results = func(url, auth=self.Auth, verify=self.VerifySsl)
+
+            # Raise an exception if we did not get a 200 response
+            results.raise_for_status()
+
+            # Convert the response to an object
+            jsonString = results.content.decode("utf-8")
+            obj = json.loads(jsonString)
+
+            return obj
+        except Exception as e:
+            raise Exception(ForemanApiWrapper.ErrorMessage_ApiCall) from e
+
+
+
---
+++ Tests/test_ForemanApiWrapper.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from ForemanApiWrapper import ForemanApiWrapper
+
+class Test_ForemanApiWrapper(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(Test_ForemanApiWrapper, self).__init__(*args, **kwargs)
+        self.username = "admin"
+        self.password = "password"
+        self.url = "https://15.4.7.1"
+        self.verifySsl = False
+
+    def test_MakeApiCall_Success(self):
+
+        api = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+
+        endpoint = "/api/environments"
+        method = "get"
+
+        api.MakeApiCall(endpoint, method)
+
+    def test_MakeApiCall_Failure_InvalidCredentials(self):
+
+        username = "fake_user"
+        password = "fake_password"
+
+        api = ForemanApiWrapper(username, password, self.url, self.verifySsl)
+
+        endpoint = "/api/environments"
+        method = "get"
+
+        with self.assertRaises(Exception) as e:
+            api.MakeApiCall(endpoint, method)
+        self.assertEqual(ForemanApiWrapper.ErrorMessage_ApiCall, e.exception.args[0])
+
+    def test_MakeApiCall_Success_CreateEnvironment(self):
+
+        api = ForemanApiWrapper(self.username, self.password, self.url, self.verifySsl)
+
+        endpoint = "/api/environments"
+        method = "post"
+        arguments =  {
+            "environment": {
+                "name": "some_environment"
+            }
+        }
+
+        response = api.MakeApiCall(endpoint, method, arguments)
+        self.asserTrue("created_at" in response.keys())
