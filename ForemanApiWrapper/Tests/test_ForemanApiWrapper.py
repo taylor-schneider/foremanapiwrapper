a28861943b43d75987198c1af422801ebe32a4bd
tschneider <tschneider@live.com>
2019-01-31 11:15:49 -0500

    #574 - Fixed import statement.

--- ForemanApiWrapper/Tests/test_ForemanApiWrapper.py
+++ ForemanApiWrapper/Tests/test_ForemanApiWrapper.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from ForemanApiWrapper.ForemanApiWrapper import ForemanApiWrapper
+from ForemanApiWrapper.ForemanApiWrapper.ForemanApiWrapper import ForemanApiWrapper
 
 class Test_ForemanApiWrapper(TestCase):
 
