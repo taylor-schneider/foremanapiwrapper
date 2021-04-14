31b0a3f6092e7b8b8bad76b579abd85ccc6c7481
Taylor Schneider <tschneider@live.com>
2021-04-14 11:05:29 -0500

    Fixing tests for subnet

--- tests/ForemanApiWrapper/ApiStateEnforcer/Test_ApiStateEnforcer_5_medium.py
+++ tests/ForemanApiWrapper/ApiStateEnforcer/Test_ApiStateEnforcer_5_medium.py
@@ -53,29 +53,62 @@ class Test_ApiStateEnforcer_5_medium(Test_ApiStateEnforcer):
             logger.warning("Utility method failed to delete medium for test case")
 
     def test__medium__create__does_not_exist(self):
-        def test__medium__create__exists(self):
-            try:
-                # Create prereqs
-                modification_receipt = Test_ApiStateEnforcer_1_smartproxy._create_smartproxy(self.api_state_enforcer)
-                smartproxy_id = modification_receipt.actual_record["smart_proxy"]["id"]
-                dns_id = smartproxy_id
-                dhcp_id = smartproxy_id
-                tftp_id = smartproxy_id
-                modification_receipt = Test_ApiStateEnforcer_2_domain._create_domain(self.api_state_enforcer, dns_id)
-                domain_id = modification_receipt.actual_record["domain"]["id"]
-                Test_ApiStateEnforcer_3_subnet._create_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id,
+        try:
+            # Create prereqs
+            modification_receipt = Test_ApiStateEnforcer_1_smartproxy._create_smartproxy(self.api_state_enforcer)
+            smartproxy_id = modification_receipt.actual_record["smart_proxy"]["id"]
+            dns_id = smartproxy_id
+            dhcp_id = smartproxy_id
+            tftp_id = smartproxy_id
+            modification_receipt = Test_ApiStateEnforcer_2_domain._create_domain(self.api_state_enforcer, dns_id)
+            domain_id = modification_receipt.actual_record["domain"]["id"]
+            Test_ApiStateEnforcer_3_subnet._create_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id,
+                                                          tftp_id)
+            Test_ApiStateEnforcer_4_architecture._create_architecture(self.api_state_enforcer)
+            # Ensure State
+            desired_state = "present"
+            minimal_record = Test_ApiStateEnforcer_5_medium._minimal_record()
+            modification_receipt = self.api_state_enforcer.ensure_state(desired_state, minimal_record)
+            self.assertTrue(modification_receipt.changed)
+        finally:
+            Test_ApiStateEnforcer_5_medium._delete_medium(self.api_state_enforcer)
+            Test_ApiStateEnforcer_4_architecture._delete_architecture(self.api_state_enforcer)
+            Test_ApiStateEnforcer_3_subnet._delete_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id,
                                                               tftp_id)
-                Test_ApiStateEnforcer_4_architecture._create_architecture(self.api_state_enforcer)
-                # Ensure State
-                desired_state = "present"
-                minimal_record = Test_ApiStateEnforcer_5_medium._minimal_record()
-                modification_receipt = self.api_state_enforcer.ensure_state(desired_state, minimal_record)
-                self.assertTrue(modification_receipt.changed)
-            finally:
-                Test_ApiStateEnforcer_5_medium._delete_medium(self.api_state_enforcer)
-                Test_ApiStateEnforcer_4_architecture._delete_architecture(self.api_state_enforcer)
-                Test_ApiStateEnforcer_3_subnet._delete_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id,
-                                                              tftp_id)
+
+    def test__medium__create__bug(self):
+        try:
+            # Create prereqs
+            modification_receipt = Test_ApiStateEnforcer_1_smartproxy._create_smartproxy(self.api_state_enforcer)
+            smartproxy_id = modification_receipt.actual_record["smart_proxy"]["id"]
+            dns_id = smartproxy_id
+            dhcp_id = smartproxy_id
+            tftp_id = smartproxy_id
+            modification_receipt = Test_ApiStateEnforcer_2_domain._create_domain(self.api_state_enforcer, dns_id)
+            domain_id = modification_receipt.actual_record["domain"]["id"]
+            Test_ApiStateEnforcer_3_subnet._create_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id, tftp_id)
+            Test_ApiStateEnforcer_4_architecture._create_architecture(self.api_state_enforcer)
+            # Create
+            desired_state = "present"
+            minimal_record = {
+                "medium": {
+                    "name": "CentOS8.3.2011 8.3.2011 ISO Mirror",
+                    "os_family": "Redhat",
+                    "path": "http://15.4.5.1:8081/CentOS/8.3.2011/os/x86_64"
+                }
+            }
+            modification_receipt = self.api_state_enforcer.ensure_state(desired_state, minimal_record)
+            self.assertTrue(modification_receipt.changed)
+            # Ensure State
+            desired_state = "present"
+            modification_receipt = self.api_state_enforcer.ensure_state(desired_state, minimal_record)
+            self.assertFalse(modification_receipt.changed)
+        finally:
+            modification_receipt = self.api_state_enforcer.ensure_state("absent", minimal_record)
+            Test_ApiStateEnforcer_4_architecture._delete_architecture(self.api_state_enforcer)
+            Test_ApiStateEnforcer_3_subnet._delete_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id, tftp_id)
+
+
 
     def test__medium__create__exists(self):
         try:
@@ -124,7 +157,6 @@ class Test_ApiStateEnforcer_5_medium(Test_ApiStateEnforcer):
             Test_ApiStateEnforcer_5_medium._delete_medium(self.api_state_enforcer)
             Test_ApiStateEnforcer_4_architecture._delete_architecture(self.api_state_enforcer)
             Test_ApiStateEnforcer_3_subnet._delete_subnet(self.api_state_enforcer, domain_id, dns_id, dhcp_id, tftp_id)
-
 
     def test__medium__delete(self):
         try:
